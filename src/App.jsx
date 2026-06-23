ddacc60a98b357b22fe155c7b6ee9b61cc1b0f50
Ahmed Ibrahim <za90.yddgio@gmail.com>
2026-06-23 18:38:53 +0300

    Fix exact path casing for LocaleLayOut

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import supabase from "./supabase/supabase.jsx";
 import { setUser } from "./Redux/Auth/AuthSLice";
 import { LinksContext } from "../src/Context/LinksContext.jsx";
 
-import LocaleLayOut from "./i18n/LocaleLayout/LocaleLayOut.jsx";LocaleLayOut
+import LocaleLayOut from "./i18n/LocaleLayout/LocaleLayOut.jsx";
 import Home from "./pages/Home";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact";
@@ -51,7 +51,7 @@ const App = () => {
             <div className="overflow-x-hidden bg-bg">
                 <Routes>
                     <Route path="/" element={<Navigate to="/home/en" replace />} />
-                    <Route path="/*" element={<LocaleLayout />}>
+                    <Route path="/*" element={<LocaleLayOut />}>
                         {/* استخدام شرط التحقق من الـ loading مع الـ Routing */}
                         {!loading && <Route path="home/:lang" element={<Home />} />}
                         <Route path="about/:lang" element={<About />} />
